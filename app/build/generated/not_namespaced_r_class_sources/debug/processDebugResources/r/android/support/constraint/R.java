a8cb87c0e685497279f555559c2eacfedb3849f9
Albin <amous4822@gmail.com>
2019-01-01 16:12:31 +0530

    tests added

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080026;
-        public static final int end = 0x7f080046;
-        public static final int gone = 0x7f080055;
-        public static final int invisible = 0x7f080060;
-        public static final int left = 0x7f080065;
-        public static final int packed = 0x7f080083;
-        public static final int parent = 0x7f080085;
-        public static final int percent = 0x7f080088;
-        public static final int right = 0x7f08008d;
-        public static final int spread = 0x7f0800b1;
-        public static final int spread_inside = 0x7f0800b2;
-        public static final int start = 0x7f0800b7;
-        public static final int top = 0x7f0800d1;
-        public static final int wrap = 0x7f0800e0;
+        public static final int bottom = 0x7f080025;
+        public static final int end = 0x7f080044;
+        public static final int gone = 0x7f080052;
+        public static final int invisible = 0x7f08005d;
+        public static final int left = 0x7f080062;
+        public static final int packed = 0x7f08007f;
+        public static final int parent = 0x7f080081;
+        public static final int percent = 0x7f080084;
+        public static final int right = 0x7f080089;
+        public static final int spread = 0x7f0800ad;
+        public static final int spread_inside = 0x7f0800ae;
+        public static final int start = 0x7f0800b3;
+        public static final int top = 0x7f0800cc;
+        public static final int wrap = 0x7f0800db;
     }
     public static final class styleable {
         private styleable() {}
---
+++ app/src/androidTest/java/com/example/amous/notekeeper/newNoteCreation.java
@@ -0,0 +1,71 @@
+package com.example.amous.notekeeper;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.core.AllOf.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class newNoteCreation {
+
+/*
+    private DataManager mDataManager;
+
+   @BeforeClass
+    public void setUp(){
+        mDataManager = DataManager.getInstance();
+    }
+*/
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivity =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNewNote() {
+
+        String testBody = "testing note body";
+        String testTitle  = "testing note title";
+        CourseInfo mTestCourse = DataManager.getInstance().getCourse("java_lang");
+
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withId(R.id.spinner_editnote)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class) , equalTo(mTestCourse))).perform(click());
+
+        onView(withId(R.id.text_notebody)).perform(typeText(testBody)).check(matches(withText(testBody)));
+        onView(withId(R.id.text_notetitle)).perform(typeText(testTitle), closeSoftKeyboard());
+
+        pressBack();
+
+
+        int lastNote = DataManager.getInstance().getNotes().size() - 1;
+        NoteInfo mTestNotes = DataManager.getInstance().getNotes().get(lastNote);
+
+        onView(withId(R.id.list_note)).perform(RecyclerViewActions.actionOnItemAtPosition(lastNote, click()));
+
+        onView(withId(R.id.text_notebody)).check(matches(withText(mTestNotes.getText())));
+        onView(withId(R.id.text_notetitle)).check(matches(withText(mTestNotes.getTitle())));
+        onView(withId(R.id.spinner_editnote)).check(matches(withSpinnerText(mTestNotes.getCourse().getTitle())));
+
+    }
+
+}
